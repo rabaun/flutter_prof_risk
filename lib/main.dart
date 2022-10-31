14b6e458012e23da1151768707a58b04294c6fdb
Andrew <swyatenco@mail.ru>
2022-10-31 17:14:37 +0300

    Change code. Add sqlite3 library windows example

--- lib/main.dart
+++ lib/main.dart
@@ -1,101 +1,105 @@
+/// This example show you how to use the openSQLiteOnWindows function
+///
+/// It also show you how to create your database file and how to execute simple
+/// SQL functions.
+///
+/// Here the table used have only one column and we use only one row to store
+/// a count value. But you can use all SQLite possibilities and build complex
+/// tables and queries, the only limitations are on SQLite side.
+/// If you have a more relevant example in mind like TodoList app or other feel
+/// free to do a pull request :)
+
+import 'dart:io' show File;
+import 'package:path_provider/path_provider.dart'
+    show getApplicationSupportDirectory;
+
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+import 'package:sqlite3/sqlite3.dart';
+import 'package:sqlite3/open.dart';
+import 'package:sqlite3_library_windows/sqlite3_library_windows.dart';
+
+late final Database db;
+
+Future<void> main() async {
+  // Override the sqlite3 load config when OS is Windows
+  open.overrideFor(OperatingSystem.windows, openSQLiteOnWindows);
+
+  // Folder where the database will be stored
+  final dbFolder = await getApplicationSupportDirectory();
+
+  // If the database file doesn't exist, create it.
+  File dbFile =
+  await File(dbFolder.path + "\\sqlite3_library_windows_example\\db")
+      .create(recursive: true);
+
+  // Open the database file
+  db = sqlite3.open(dbFile.path);
+
+  // Create 'count' table if the table doesn't already exist
+  db.execute('CREATE TABLE IF NOT EXISTS '
+      'count (count_value INTEGER NOT NULL);');
+
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
   void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+    setState(() => _counter++);
+    _updateCounterInDatabase();
+  }
+
+  void _getCounterFromDatabase() {
+    var values = db.select('SELECT count_value FROM count;');
+    if (values.isNotEmpty) _counter = values.first['count_value'];
+  }
+
+  void _updateCounterInDatabase() {
+    db.execute('DELETE FROM count;');
+    db.execute('INSERT INTO count (count_value) VALUES ($_counter);');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getCounterFromDatabase();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
+            Text(
               'You have pushed the button this many times:',
             ),
             Text(
@@ -108,8 +112,8 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
